61b9c6479a20d68d18dae56018b6e22336036221
Allan Bennett <allanbts.18music@gmail.com>
2024-07-31 22:10:13 -0500

    feat: add song item

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,8 +6,17 @@ import { InputComponent } from './input/input.component';
 import { FormsModule } from '@angular/forms';
 import { TabComponent } from './tab/tab.component';
 import { SelectBoxComponent } from './select-box/select-box.component';
+import { SongItemComponent } from './song-item/song-item.component';
 
-const items = [HeaderComponent,InputComponent,TabComponent,SelectBoxComponent]
+const items = [
+  HeaderComponent,
+  InputComponent,
+  TabComponent,
+  SelectBoxComponent,
+  HeaderComponent,
+  SongItemComponent,
+  
+]
 
 @NgModule({
   declarations: [items],
