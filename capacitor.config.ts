db0df3eabbbc708ea5ea9f82015f54e8a334bcde
Allan Bennett <allanbts.18music@gmail.com>
2026-05-18 20:47:29 -0500

    migrate to ionic 8

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'idea.tracks.composer',
   appName: 'IdeaTracks',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
