b84a26057943a848fb90d12a35de1d242eebb252
Allan Bennett <allanbts.18music@gmail.com>
2026-05-25 23:23:25 -0500

    fix permisions

--- android/app/src/main/java/idea/tracks/composer/MainActivity.java
+++ android/app/src/main/java/idea/tracks/composer/MainActivity.java
@@ -1,5 +1,19 @@
 package idea.tracks.composer;
 
+import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+        }
+    }
+}
